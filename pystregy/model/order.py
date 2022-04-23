9db55b051838ec183377fe37f120e1f8fa3f7d65
Nikolay <zadneprovskiyWork@gmail.com>
2022-04-23 18:50:58 +0300

    fix

--- pystregy/model/order.py
+++ pystregy/model/order.py
@@ -1,19 +1,12 @@
 from enum import IntEnum
 
-class Status(IntEnum):
-    CREATED = 0
-    ACCEPTED = 1
-    PARTIAL = 2
-    COMPLETED = 3
-    CANCELLED = 4
-    EXPIRED = 5
-    MARGIN = 6
+class Order:
+    CREATED, ACCEPTED, PARTIAL, COMPLETED, CANCELLED, EXPIRED, MARGIN = range(7)
 
-class Order():
     __slots__ = 'id', 'price', 'size', 'status'
 
     def __init__(self, id: int, price: float, size: float):
         self.id = id
         self.price = price
         self.size = size
-        self.status = Status.CREATED
+        self.status = self.CREATED
--- pystregy/model/position.py
+++ pystregy/model/position.py
@@ -2,13 +2,9 @@ from enum import IntEnum
 
 from .order import Order
 
-class Status(IntEnum):
-     OPEN = 0
-     TAKE_PROFIT = 1
-     STOP_LOSS = 2
-     CANCELLED = 3
-
 class Position():
+    OPEN, TAKE_PROFIT, STOP_LOSS, CANCELLED = range(4)
+
     __slots__ = 'id', 'main_order', 'take_order', 'stop_order', 'status'
 
     def __init__(self, 
@@ -21,4 +17,4 @@ class Position():
         self.main_order = main_order
         self.take_order = take_order
         self.stop_order = stop_order
-        self.status = Status.OPEN
+        self.status = self.OPEN
